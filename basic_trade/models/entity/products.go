d6e72befedad0ab0786bdfd6715183ba0242987b
annisa <ahayuwidya@gmail.com>
2024-06-14 18:31:39 +0700

    valid tags

--- basic_trade/models/entity/products.go
+++ basic_trade/models/entity/products.go
@@ -5,7 +5,7 @@ import "time"
 type Product struct {
 	ID        uint `gorm:"primaryKey"`
 	UUID      string
-	Name      string `json:"name"`
+	Name      string `json:"name" valid:"required,alpha"`
 	ImageURL  string `json:"image_url"`
 	AdminID   uint   `json:"admin_id"`
 	CreatedAt *time.Time
--- basic_trade/models/entity/variants.go
+++ basic_trade/models/entity/variants.go
@@ -5,8 +5,8 @@ import "time"
 type Variant struct {
 	ID          uint `gorm:"primaryKey"`
 	UUID        string
-	VariantName string `json:"variant_name" form:"variant_name"`
-	Quantity    uint   `json:"quantity" form:"quantity"`
+	VariantName string `json:"variant_name" form:"variant_name" valid:"required,alpha"`
+	Quantity    uint   `json:"quantity" form:"quantity" valid:"numeric"`
 	ProductID   uint   `json:"product_id" form:"product_id"`
 	CreatedAt   *time.Time
 	UpdatedAt   *time.Time
